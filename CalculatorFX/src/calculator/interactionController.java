c240068170bf00a75739c43bc72316ca46ffd177
FAUSTO <FAUSTO@FAUSTO-PC>
2019-09-22 10:26:24 +0200

    Numbers can now be written to the text field

--- CalculatorFX/src/calculator/interactionController.java
+++ CalculatorFX/src/calculator/interactionController.java
@@ -1,13 +1,19 @@
 package calculator;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class interactionController {
 
+	@FXML
+	private TextField outputField;
+	
 	public void onNumberPressed(ActionEvent event) {
 		Button pressedButton = (Button) event.getSource();
-		System.out.println("Debug: "+pressedButton.getText());
-		
+		String chosenNumber = pressedButton.getText();
+		System.out.println("Debug: "+chosenNumber);
+		outputField.setText(outputField.getText()+chosenNumber);
 	}
 }
