fec2b45547134095e7f4cf4a94063cc9da02790b
FAUSTO <FAUSTO@FAUSTO-PC>
2019-09-22 10:11:00 +0200

    Added a simple print to console for number buttons

--- CalculatorFX/src/calculator/interactionController.java
+++ CalculatorFX/src/calculator/interactionController.java
@@ -1,5 +1,13 @@
 package calculator;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class interactionController {
-	//TODO
+
+	public void onNumberPressed(ActionEvent event) {
+		Button pressedButton = (Button) event.getSource();
+		System.out.println("Debug: "+pressedButton.getText());
+		
+	}
 }
