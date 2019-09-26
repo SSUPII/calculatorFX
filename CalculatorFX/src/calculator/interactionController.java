b2c20c7ba0f50430670587e23b4284aacc403e1c
FAUSTO <FAUSTO@FAUSTO-PC>
2019-09-26 21:30:05 +0200

    changed the output object from a TextField to a Label

--- CalculatorFX/src/calculator/interactionController.java
+++ CalculatorFX/src/calculator/interactionController.java
@@ -4,138 +4,139 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
 public class interactionController {
 
 	@FXML
-	private TextField outputField;
-	
+	private Label outputLabel;
+
 	@FXML
 	private Label errorNotifierLabel;
-	
+
 	private Operation currentCalculation = new Operation();
 	private boolean isSecondNumber = false;
-	
+
 	public void onMainButtonPressed(ActionEvent event) {
 		Button pressedButton = (Button) event.getSource();
 		String chosenChar = pressedButton.getText();
-		outputField.appendText(chosenChar);
+		outputLabel.setText(outputLabel.getText()+chosenChar);
 	}
-	
+
 	public void onSignPressed(ActionEvent event) {
 		Button pressedButton = (Button) event.getSource();
 		try {
 			switch (pressedButton.getId()) {
 			case "sum":{
 				prepareOperation(Operation.SUM);
-				outputField.setText("");
+				outputLabel.setText("");
 				break;
 			}
 			case "subtraction":{
 				prepareOperation(Operation.SUB);
-				outputField.setText("");
+				outputLabel.setText("");
 				break;
 			}
 			case "multiplication":{
 				prepareOperation(Operation.MUL);
-				outputField.setText("");
+				outputLabel.setText("");
 				break;
 			}
 			case "division":{
 				prepareOperation(Operation.DIV);
-				outputField.setText("");
+				outputLabel.setText("");
 				break;
 			}
 			}
 		}
 		catch (InvalidNumberException error) {
-			
+
 		}
 	}
-	
+
 	public void onEqualsButtonPressed() {
 		double result = 0;
 		result = calculate();
-		outputField.setText(Double.toString(result));
+		outputLabel.setText(Double.toString(result));
 	}
-	
+
 	private void prepareOperation(byte sign) throws InvalidNumberException {
 		if(isSecondNumber) {
-			currentCalculation.setSecondNumber(outputField.getText());
+			currentCalculation.setSecondNumber(outputLabel.getText());
 			calculate(sign);
 		}
 		else {
-			currentCalculation.setFirstNumber(outputField.getText());
+			currentCalculation.setFirstNumber(outputLabel.getText());
 			currentCalculation.setSign(sign);
 			isSecondNumber = true;
 		}
 	}
-	
+
 	public void onBackspaceButtonPressed() {
 		try {
-			String newValue = outputField.getText(0,outputField.getText().length()-1);
-			outputField.setText(newValue);
+			String newValue = "";
+			for(int n = 0;outputLabel.getText().length()-1>n;n++)
+				newValue += outputLabel.getText().charAt(n);
+			outputLabel.setText(newValue);
 		}
 		catch (Exception e) {
 			errorNotifierLabel.setText("Error - Nothing to delete");
 			errorNotifierLabel.setTextFill(Color.web("#FF3333"));
 		}
 	}
-	
+
 	public void onClearButtonPressed() {
 		currentCalculation = new Operation();
 		isSecondNumber = false;
-		outputField.setText("");
-		
+		outputLabel.setText("");
+
 		errorNotifierLabel.setText("Ready");
 		errorNotifierLabel.setTextFill(Color.web("#AAAAAA"));
 	}
-	
+
 	public void onClearEntryButtonPressed() {
 		//CE stands for Clear Entry
-		outputField.setText("");
+		outputLabel.setText("");
 	}
-	
+
 	public void onInvertButtonPressed() {
-		Double newNumber = Double.parseDouble(outputField.getText())*(-1);
-		
-		outputField.setText(Double.toString(newNumber));
+		Double newNumber = Double.parseDouble(outputLabel.getText())*(-1);
+
+		outputLabel.setText(Double.toString(newNumber));
 	}
-	
+
 	private double calculate() {
 		double result = 0;
 		try {
-			currentCalculation.setSecondNumber(outputField.getText());
-			
+			currentCalculation.setSecondNumber(outputLabel.getText());
+
 			result = currentCalculation.compute();
 			currentCalculation = new Operation();
 			isSecondNumber = false;
-			
+
 		} catch (Exception e) {
 			errorNotifierLabel.setText("Error - "+e.getMessage());
 			errorNotifierLabel.setTextFill(Color.web("#FF3333"));
-			
+
 		}
-		
+
 		return result;
 	}
-	
+
 	private double calculate(byte newSign) {
-		double result = Double.parseDouble(outputField.getText());
+		double result = Double.parseDouble(outputLabel.getText());
 		try {
 			result = currentCalculation.compute();
 			currentCalculation = new Operation();
 			isSecondNumber = false;
-			
+
 		} catch (Exception e) {
 			errorNotifierLabel.setText("Error - "+e.getMessage());
 			errorNotifierLabel.setTextFill(Color.web("#FF3333"));
-			
+
 		}
-		
+
 		return result;
 	}
-	
+
 }
